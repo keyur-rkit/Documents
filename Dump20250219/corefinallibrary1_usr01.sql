92bf9148f62210cc20007ca1c495c9fb712a666a
keyursaradva <saradvakeyur@gmail.com>
2025-03-06 11:31:20 +0530

    dump

---
+++ Dump20250219/corefinallibrary1_usr01.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: corefinallibrary1
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usr01`
+--
+
+DROP TABLE IF EXISTS `usr01`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usr01` (
+  `R01F01` int NOT NULL AUTO_INCREMENT COMMENT 'UserId',
+  `R01F02` varchar(50) NOT NULL COMMENT 'Username',
+  `R01F03` varchar(255) NOT NULL COMMENT 'Password',
+  `R01F04` varchar(50) NOT NULL COMMENT 'Role',
+  `R01F05` datetime NOT NULL COMMENT 'CreatedAt',
+  `R01F06` datetime NOT NULL COMMENT 'UpdatedAt',
+  PRIMARY KEY (`R01F01`),
+  UNIQUE KEY `R01F02` (`R01F02`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Users Table';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usr01`
+--
+
+LOCK TABLES `usr01` WRITE;
+/*!40000 ALTER TABLE `usr01` DISABLE KEYS */;
+INSERT INTO `usr01` VALUES (2,'string','string','User','2025-02-10 15:59:37','2025-02-10 15:59:37'),(3,'admin','password','Admin','2025-02-11 10:27:35','2025-02-11 10:27:35'),(4,'user','password','Admin','2025-02-11 10:27:46','2025-02-11 10:28:15'),(5,'keyurA','password','Admin','2025-02-11 10:28:28','2025-02-11 10:28:28'),(6,'keyurU','password','User','2025-02-11 10:28:39','2025-02-11 10:28:39');
+/*!40000 ALTER TABLE `usr01` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-19 14:47:18
